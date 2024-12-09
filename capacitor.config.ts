802d7fad4f078502be681f6d1dab7463bc10b29c
Niklas Trapp <niklas031712@gmail.com>
2024-12-09 08:35:34 +0100

    add new packages

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ionic.hopfly',
+  appId: 'net.ntrapp.hopfly',
   appName: 'hopfly',
   webDir: 'www'
 };
