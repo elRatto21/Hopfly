80511c6993c0b6d93c6180ea2abcf746353c5860
Niklas Trapp <niklas031712@gmail.com>
2024-12-09 15:50:42 +0100

    some dumb map stuff idk

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
@@ -8,13 +8,21 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class MapPage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  @ViewChild('map')
+  mapRef!: any;
+  newMap!: any;
+
+  async createMap() {
   }
 
 }
