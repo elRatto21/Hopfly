a9695ccc1557709b3db574fa50a92390b5013d9f
Niklas Trapp <niklas031712@gmail.com>
2024-12-12 10:48:21 +0100

    add entry markers to map

--- src/app/components/map/map/map-comp.component.ts
+++ src/app/components/map/map/map-comp.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
+import { Entry } from 'src/app/models/entry.model';
+import { EntryService } from 'src/app/services/entry.service';
 
 @Component({
   selector: 'app-map-comp',
@@ -12,14 +14,31 @@ export class MapComponent implements AfterViewInit {
   private map!: L.Map;
   private markers: L.Marker[] = [];
 
+  private entries: Entry[] = [];
+
   private defaultCenter: L.LatLngTuple = [47.508820319182774, 7.616206062342946];
   private userMarker!: L.Marker;
-  constructor() { }
+  constructor(private entryService: EntryService) {
+    const iconUrl = 'assets/leaflet/images/marker-icon.png';
+    const shadowUrl = 'assets/leaflet/images/marker-shadow.png';
+    const iconDefault = L.icon({
+      iconUrl,
+      shadowUrl,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      tooltipAnchor: [16, -28],
+      shadowSize: [41, 41]
+    });
+    L.Marker.prototype.options.icon = iconDefault;
+  }
 
-  ngAfterViewInit(): void {
+  async ngAfterViewInit() {
     setTimeout(() => {
       this.initializeMap();
     }, 50)
+    this.entries = await this.entryService.getAllEntries();
+    this.addEntryMarkers();
   }
 
   private async initializeMap() {
@@ -40,31 +59,17 @@ export class MapComponent implements AfterViewInit {
     }).addTo(this.map);
   }
 
-  private addDrinkMarkers(): void {
-    const customIcon = L.icon({
-      iconUrl: 'assets/marker-icon.png',
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-    });
-
-    /*this.drinkEntries.forEach(entry => {
-      const marker = L.marker([entry.latitude, entry.longitude], { icon: customIcon })
-        .bindPopup(`<b>${entry.name}</b>`)
+  private addEntryMarkers(): void {
+    this.entries.forEach(entry => {
+      const marker = L.marker([entry.location.latitude, entry.location.longitude])
+        .bindPopup(`<b>${entry.beer_name}</b>`)
         .addTo(this.map);
 
       this.markers.push(marker);
-    });*/
+    });
   }
 
-  public addMarker(entry: any): void {
-    const marker = L.marker([entry.latitude, entry.longitude])
-      .bindPopup(`<b>${entry.name}</b>`)
-      .addTo(this.map);
 
-    this.markers.push(marker);
-    //this.drinkEntries.push(entry);
-  }
 
   public centerMap(latitude: number, longitude: number, zoom: number = 13): void {
     this.map.setView([latitude, longitude], zoom);
