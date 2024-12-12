6d998002772dde4761aa6a4580745a00a0f3fe49
Niklas Trapp <niklas031712@gmail.com>
2024-12-12 15:40:35 +0100

    remove reminder logic from settings page

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -5,99 +5,32 @@ import { LocalNotifications } from '@capacitor/local-notifications';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { addIcons } from 'ionicons';
+import { alarm, contrast, moon, sunny } from 'ionicons/icons';
+import { ReminderSettingsComponent } from "../../components/settings/reminder-settings/reminder-settings.component";
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonicModule, FormsModule, CommonModule]
+  imports: [IonicModule, FormsModule, CommonModule, ReminderSettingsComponent]
 })
 export class SettingsPage implements OnInit {
-  isDailyReminderEnabled: boolean = false;
   darkmode: boolean = false
 
-  constructor() { }
+  constructor() { addIcons({
+    alarm, moon, sunny
+  }) }
 
   async ngOnInit() {
     this.darkmode = Boolean(localStorage.getItem("darkmode") === "true")
-    await this.requestPermissions();
-
-    const pending = await LocalNotifications.getPending();
-    this.isDailyReminderEnabled = pending.notifications.length > 0;
+ 
   }
 
   toggleDarkmode(e: any) {
     document.documentElement.classList.toggle("ion-palette-dark", e.detail.checked)
     localStorage.setItem("darkmode", e.detail.checked)
-  }
-
-  async requestPermissions() {
-    try {
-      const result = await LocalNotifications.requestPermissions();
-    } catch (error) {
-      console.error('Error requesting notification permissions:', error);
-    }
-  }
-
-  async toggleDailyReminder(event: any) {
-    const isEnabled = event.detail.checked;
-    
-    if (isEnabled) {
-      await this.scheduleDailyNotification();
-    } else {
-      await this.cancelDailyNotification();
-    }
-  }
-
-  async scheduleDailyNotification() {
-    try {
-      const now = new Date();
-      const nextNotification = new Date(
-        now.getFullYear(),
-        now.getMonth(),
-        now.getDate(),
-        20,
-        0,
-        0
-      );
-      
-      if (now.getHours() >= 20) {
-        nextNotification.setDate(nextNotification.getDate() + 1);
-      }
-
-      await LocalNotifications.schedule({
-        notifications: [
-          {
-            id: 1,
-            title: 'Hopfly Daily Reminder',
-            body: 'Time to log your drink!',
-            schedule: {
-              at: nextNotification,
-              repeats: true,
-              every: 'day'
-            },
-            sound: "undefined",
-            actionTypeId: '',
-            extra: null
-          }
-        ]
-      });
-
-
-    } catch (error) {
-      console.error('Error scheduling notification:', error);
-      this.isDailyReminderEnabled = false;
-    }
-  }
-
-  async cancelDailyNotification() {
-    try {
-      await LocalNotifications.cancel({ notifications: [{ id: 1 }] });
-    } catch (error) {
-      console.error('Error cancelling notification:', error);
-      this.isDailyReminderEnabled = true;
-    }
   }
 
 }
