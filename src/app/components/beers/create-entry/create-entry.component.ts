ccddb9925710b8cd2e84625accc9d667741dcdfd
Niklas Trapp <niklas031712@gmail.com>
2024-12-12 15:38:48 +0100

    use high accuracy for location

--- src/app/components/beers/create-entry/create-entry.component.ts
+++ src/app/components/beers/create-entry/create-entry.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+// src/app/components/create-entry/create-entry.component.ts
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { addIcons } from 'ionicons';
 import { add, close, camera, location, checkmark } from 'ionicons/icons';
@@ -7,10 +8,9 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angula
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Geolocation } from '@capacitor/geolocation';
 import { CommonModule } from '@angular/common';
-import { Brand } from '../../../models/brand.model'
+import { Brand } from '../../../models/brand.model';
 import { BrandService } from 'src/app/services/brand.service';
 import { ImageService } from 'src/app/services/image.service';
-import { Image } from 'src/app/models/image.model';
 import { EntryService } from 'src/app/services/entry.service';
 
 @Component({
@@ -22,11 +22,14 @@ import { EntryService } from 'src/app/services/entry.service';
 })
 export class CreateEntryComponent implements OnInit {
   @ViewChild(IonModal) modal!: IonModal;
+  @Input() entry?: any; // For edit mode
+  @Input() trigger?: string; // Modal trigger ID
+
   beerForm!: FormGroup;
   location: { latitude: number; longitude: number } | null = null;
   imageString: string | undefined;
-
-  beerBrands: Brand[] = []
+  beerBrands: Brand[] = [];
+  isEditMode = false;
 
   constructor(
     private fb: FormBuilder,
@@ -38,18 +41,32 @@ export class CreateEntryComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.initForm();
     this.brandService.getBrands().then((data) => {
-      this.beerBrands = data
+      this.beerBrands = data;
     });
+    
+    this.isEditMode = !!this.entry;
+    this.initForm();
   }
 
   initForm() {
-    this.beerForm = this.fb.group({
-      beer_name: ['', Validators.required],
-      brand_id: ['', Validators.required],
-      size: ['', [Validators.required, Validators.min(0)]],
-    });
+    if (this.isEditMode && this.entry) {
+      // Edit mode - populate form with existing data
+      this.beerForm = this.fb.group({
+        beer_name: [this.entry.beer_name, Validators.required],
+        brand_id: [this.entry.brand_id, Validators.required],
+        size: [this.entry.size, [Validators.required, Validators.min(0)]],
+      });
+      this.location = this.entry.location;
+      // Don't set imageString as we want to show the old image until a new one is taken
+    } else {
+      // Create mode - empty form
+      this.beerForm = this.fb.group({
+        beer_name: ['', Validators.required],
+        brand_id: ['', Validators.required],
+        size: ['', [Validators.required, Validators.min(0)]],
+      });
+    }
   }
 
   async takePicture() {
@@ -61,7 +78,7 @@ export class CreateEntryComponent implements OnInit {
         source: CameraSource.Camera
       });
 
-      this.imageString = image.base64String
+      this.imageString = image.base64String;
     } catch (error) {
       console.error('Camera error:', error);
     }
@@ -69,7 +86,7 @@ export class CreateEntryComponent implements OnInit {
 
   async getLocation() {
     try {
-      const position = await Geolocation.getCurrentPosition();
+      const position = await Geolocation.getCurrentPosition({ enableHighAccuracy: true });
       this.location = {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
@@ -81,32 +98,38 @@ export class CreateEntryComponent implements OnInit {
 
   cancel() {
     this.modal.dismiss(null, 'cancel');
-    this.initForm();
-    this.imageString = undefined
-    this.location = null
+    this.reset();
   }
 
   async submit() {
     const formData = { ...this.beerForm.value };
-    if (this.imageString != undefined) {
-      const image: any | null = await this.imageService.createEntry(this.imageString)
-      console.log(image)
-      formData.image_id = image.id
+
+    if (this.imageString) {
+      const image: any = await this.imageService.createEntry(this.imageString);
+      formData.image_id = image.id;
     }
 
     if (this.location) {
-      formData.location = this.location
+      formData.location = this.location;
     }
 
     if (this.beerForm.valid) {
-      this.entryService.createEntry(formData);
+      if (this.isEditMode) {
+        await this.entryService.updateEntry(this.entry.id, formData);
+      } else {
+        await this.entryService.createEntry(formData);
+      }
       this.modal.dismiss(formData, 'confirm');
     }
   }
 
   onWillDismiss(event: Event) {
+    this.reset();
+  }
+
+  private reset() {
     this.initForm();
-    this.imageString = undefined
+    this.imageString = undefined;
     this.location = null;
   }
 }
