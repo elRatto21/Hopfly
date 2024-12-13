07979b1d7d376787d41b1daf72aba2f9cacf1662
Niklas Trapp <niklas031712@gmail.com>
2024-12-13 10:03:21 +0100

    add sorting to entry list

--- src/app/components/beers/entry-list/entry-list.component.ts
+++ src/app/components/beers/entry-list/entry-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EntryService } from 'src/app/services/entry.service';
 import { IonicModule } from '@ionic/angular'
 import { addIcons } from 'ionicons';
-import { image, pencil, trash } from 'ionicons/icons';
+import { image, location, pencil, trash } from 'ionicons/icons';
 import { CommonModule } from '@angular/common';
 import { EntryCardComponent } from '../entry-card/entry-card.component';
 import { Brand } from 'src/app/models/brand.model';
@@ -22,7 +22,7 @@ export class EntryListComponent implements OnInit {
 
   constructor(private entryService: EntryService, private brandService: BrandService) {
     addIcons({
-      trash, image, pencil
+      trash, image, pencil, location
     })
   }
 
@@ -35,6 +35,11 @@ export class EntryListComponent implements OnInit {
     this.entryService.getAllEntries().then((data) => {
       console.log("entries:", data)
       this.entries = data;
+
+      const select = document.querySelector('ion-select');
+      if (select && select.value && select.value !== 'none') {
+        this.sortEntries({ detail: { value: select.value } });
+      }
     })
   }
 
@@ -61,6 +66,34 @@ export class EntryListComponent implements OnInit {
   public async refreshEntries() {
     await this.loadEntries();
     await this.loadBrands();
+  }
+
+  sortEntries(event: any) {
+    const sortType = event.detail.value;
+
+    if (sortType === 'none') {
+      this.entries.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
+      return;
+    }
+
+    this.entries.sort((a, b) => {
+      switch (sortType) {
+        case 'date-asc':
+          return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
+        case 'date-desc':
+          return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+        case 'brand-asc':
+          return this.getBrandById(a.brand_id).localeCompare(this.getBrandById(b.brand_id));
+        case 'brand-desc':
+          return this.getBrandById(b.brand_id).localeCompare(this.getBrandById(a.brand_id));
+        case 'size-asc':
+          return a.size - b.size;
+        case 'size-desc':
+          return b.size - a.size;
+        default:
+          return 0;
+      }
+    });
   }
 
 }
