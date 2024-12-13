eabbfee003355d9d06b3c2b745da359c8c8a36a2
Niklas Trapp <niklas031712@gmail.com>
2024-12-13 10:13:27 +0100

    add splashscreen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,25 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'net.ntrapp.hopfly',
+  appId: 'com.niklas.trapp.hopfly',
   appName: 'Hopfly',
   webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+    }
+  }
 };
 
 export default config;
